3a3a0765ba696944ff37b37f095e085088f4143d
Claudia <claudiamongetorres@gmail.com>
2016-06-23 21:23:53 +0000

    Initial commit

---
+++ game.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	var questions = ['How many countries participated in the first World Cup in 1930?','Which team has managed to win the UEFA European Championship and added a World Cup victory to the honor?','On what NBA Draft was Kobe Bryant elected?', 'Who is the NBA player with most championship titles?','The first four games of a volleyball match are played to what score?','How many players are there on one side of a volleyball court?','In volleyball,to win the game, a team must be ahead in points by how many points?','Which position player is known as "1" on the baseball diamond?','In baseball,which is a full-count?','How many teams are in the MLB?','Whats the name of the NBA team in Oklahoma?']
+	$('#startGame').click(function(event){
+		event.preventDefault();
+		console.log('13 countries')
+
+	});
+});
